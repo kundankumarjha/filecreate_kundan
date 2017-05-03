49d393d00a66869b4a64ef5a30f1a7a20a535936
Kundan Jha <kjha@tibco.com>
2017-05-03 21:42:22 +0530

    test submit

---
+++ filecreate/runtime/activity.go
@@ -0,0 +1,51 @@
+package filecreate
+
+import (
+	"os"
+
+	"io"
+
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+// log is the default package logger
+var log = logger.GetLogger("activity-pankaj-file")
+
+const (
+	ivFilename = "fileName"
+	ovCreated  = "isCreated"
+)
+
+type fileActivity struct {
+	metadata *activity.Metadata
+}
+
+// init create & register activity
+func init() {
+	md := activity.NewMetadata(jsonMetadata)
+	activity.Register(&fileActivity{metadata: md})
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *fileActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *fileActivity) Eval(context activity.Context) (done bool, err error) {
+
+	fileName := context.GetInput(ivFilename).(string)
+	content := "Hello file activity"
+	file, err := os.Create(fileName)
+	defer file.Close()
+	io.WriteString(file, content)
+
+	if err != nil {
+		context.SetOutput(ovCreated, false)
+		return true, err
+	}
+	context.SetOutput(ovCreated, true)
+
+	return true, nil
+}
---
+++ filecreate/runtime/activity_metadata.go
@@ -0,0 +1,20 @@
+package filecreate
+
+var jsonMetadata = `{
+  "name": "pankaj-file",
+  "version": "0.0.1",
+  "description": "Test activity to create a file",
+  "inputs":[
+    {
+      "name": "fileName",
+      "type": "string",
+      "required": true
+    }
+  ],
+  "outputs": [
+    {
+      "name": "isCreated",
+      "type": "boolean"
+    }
+  ]
+}`
